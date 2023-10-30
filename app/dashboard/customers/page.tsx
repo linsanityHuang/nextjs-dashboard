b9b0237f7198a51db9bf7be4f8b38286cc4af3b0
huangjs3 <huangjs3@lenovo.com>
2023-10-30 22:36:32 +0800

    feat(customer): add customer list

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,14 +1,31 @@
+import Pagination from '@/app/ui/invoices/pagination';
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
-
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
-export default function() {
-    return (
-        <>
-            <p>Customers Pages</p>
-        </>
-    );
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?:string;
+    }
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+        <Table customers={customers} />
+        <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={customers.length} /> */}
+      </div>
+    </div>
+  );
 }
